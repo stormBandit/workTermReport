1dc62e2b94af76daa33f1bc6de6844b9ad565fa2
dalton <dalton.polhill@tulip.io>
2017-08-09 09:59:52 -0400

    created the header.html page for holding the navigation bar and <head> tags

--- index.php
+++ index.php
@@ -1,62 +1,112 @@
-<HTML xmlns="http://www.w3.org/1999/html">
-    <HEAD>
-        <link rel="stylesheet" href="css/*">
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </HEAD>
+<?php include("includes/header.html"); ?>
 
-    <title>Welcome</title>
+<body>
 
-    <body>
+    <!-- Page Content -->
+    <div class="container">
 
-        <nav class="navbar navbar-default">
-
-            <div class="toolBarButton-wrapper"> <form method="post" action="login.php"> <button type="submit" class="toolBarButton">Home</button> </form> </div>
-            <div class="toolBarButton-wrapper"> <form method="post"> <button formaction="#newsfeed" class="toolBarButton">Newsfeed</button> </form> </div>
-            <div class="toolBarButton-wrapper"> <form method="post"> <button formaction="#messages" class="toolBarButton">Messages</button> </form> </div>
-            <div class="toolBarButton-wrapper"> <form method="post"> <button formaction="#contacts" class="toolBarButton">Contacts</button> </form> </div>
-        </nav>
-
-        <section style="max-width:600px">
-            <h2>THE BAND</h2>
-            <p><i>We love music</i></p>
-        </section>
-
-        <section style="max-width:600px">
-            <p>We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </section>
-
-        <p>Here are some 'lightbox' examples [ <a href="#example1">Open example #1</a> | <a href="#example2">Open example #2</a> ]</p>
-
-        <div class="lightbox" id="example1">
-            <figure>
-                <a href="#" class="closeButton"></a>
-                <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis enim, placerat id eleifend eu, semper vel sem. Sed interdum commodo enim venenatis pulvinar. Proin mattis lorem vitae diam scelerisque hendrerit. Fusce cursus imperdiet mauris, vitae hendrerit velit dignissim a. Suspendisse potenti. Aenean feugiat facilisis diam, in posuere sapien mattis vel. Proin molestie rutrum diam, pharetra feugiat ligula sollicitudin sed. Etiam cursus diam quis tellus aliquam gravida. Aliquam erat volutpat.<br />
-                    Etiam varius adipiscing mi eget imperdiet. Nulla quis vestibulum leo. Integer molestie massa ut massa commodo in blandit purus aliquam. Mauris sit amet posuere massa. Ut a eleifend augue. Proin sodales mauris nec tellus pharetra dictum.</figcaption>
-            </figure>
+        <!-- Portfolio Item Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Dalton Polhill
+                    <small>Coop Workterm Reports</small>
+                </h1>
+            </div>
         </div>
+        <!-- /.row -->
 
-        <div class="lightbox" id="example2">
-            <figure>
-                <a href="#" class="closeButton"></a>
-                <figcaption>Cras risus odio, pharetra nec ultricies et, mollis ac augue. Nunc et diam quis sapien dignissim auctor. Quisque quis neque arcu, nec gravida magna. Etiam ullamcorper augue quis orci posuere et tincidunt augue semper. Maecenas varius augue eu orci auctor bibendum tristique ligula egestas. Morbi pharetra tortor iaculis erat porta id aliquam leo cursus. Ut nec elit vel mauris dapibus lacinia eget sed odio.</figcaption>
-            </figure>
+        <!-- Portfolio Item Row -->
+        <div class="row">
+
+            <div class="col-md-8">
+                <img class="img-responsive" src="http://placehold.it/750x500" alt="">
+            </div>
+
+            <div class="col-md-4">
+                <h3>University of Guelph Coop Program</h3>
+                <p>The University of Guelph - Computer Science Department offers and amazing coop education program that I am glad to be apart of.
+                </p>
+                <h3>Project Details</h3>
+                <ul>
+                    <li>Lorem Ipsum</li>
+                    <li>Dolor Sit Amet</li>
+                    <li>Consectetur</li>
+                    <li>Adipiscing Elit</li>
+                </ul>
+            </div>
+
         </div>
+        <!-- /.row -->
 
-        <form method="post" action="login.php">
-            <button type="submit" class="mainButton">Sign In / Register</button>
-        </form>
+        <!-- Related Projects Row -->
+        <div class="row">
 
+            <div class="col-lg-12">
+                <h3 class="page-header">Coop Placements</h3>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#tulip-retail">
+                    <h3 style="margin-top: -10px"><small>Term #1 - Summer 2017</small></h3>
+                    <img class="img-responsive portfolio-item" src="images/tulip-retail.png" alt="">
+                </a>
+            </div>
+
+            <!-- save for future terms
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <h3 style="margin-top: -10px"><small>Term #2</small></h3>
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <h3 style="margin-top: -10px"><small>Term #3</small></h3>
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <h3 style="margin-top: -10px"><small>Term #4</small></h3>
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <h3 style="margin-top: -10px"><small>Term #5</small></h3>
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            -->
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
         <footer>
-            <ul style="list-style-type: lower-greek; list-style-position: outside;">
-                <li> <a href="#facebook"> <i> Facebook</i> </a> </li>
-                <li> <a href="#twitter"> <i>Twitter</i> </a> </li>
-                <li> <a href="#instagram"> <i>Instagram</i> </a> </li>
-                <li> <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a> </p> </li>
-            </ul>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Dalton Polhill 2017</p>
+                </div>
+            </div>
+            <!-- /.row -->
         </footer>
-        </footer>
-    </body>
 
-</HTML>
+    </div>
+    <!-- /.container -->
 
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
