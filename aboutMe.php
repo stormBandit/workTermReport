e3e474a759c2285b7fbac156611eae3cfa9fe225
dalton <dalton.polhill@tulip.io>
2017-08-10 11:23:42 -0400

    added the projects section to the about me page

--- aboutMe.php
+++ aboutMe.php
@@ -1,10 +1,41 @@
 <?php include("includes/header.html"); ?>
 
-    <h1>Dalton Polhill</h1>
-    <p>I am coop student at the university of Guelph, currently working towards a bachelors of computing
-        with a major in Computer Science and a Minor in Business.
-        I have a particular interest in software security....to be continued
-    </p>
+    <div class="container">
+        <h1 class="pager">Dalton Polhill</h1>
+        <p class="pager">I am coop student at the university of Guelph, currently working towards a bachelors of computing
+            with a major in Computer Science and a Minor in Business. I have just completed my first work term
+            with Tulip Retail in Toronto. Throughout the 4 month work term I was able to use many of the skills I learned
+            in the classroom to assist me in accomplishing tasks in timely manners.
+            I had an opportunity to apply my knowledge in a hands on environment, which I feel is extremely
+            valuable and will help me grow in my career.
+            I hope to use my future work terms to try out new areas of the industry and expand my knowledge,
+            I have a particular interest in cyber security, hardware, artificial intelligence systems and code optimizations.
+        </p>
+
+        <!-- Related Projects Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h3 class="page-header">Projects</h3>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <h3 style="margin-top: -10px"><small>OWASP - Juice Shop</small></h3>
+                <a href="#juice-shop-details">Project Details</a>
+                <br>
+                <a href="https://github.com/stormBandit/juice-shop">See Project on Github</a>
+                <img class="img-responsive portfolio-item" src="images/JuiceShop_Logo.png" alt="">
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <h3 style="margin-top: -10px"><small>SunFunder - Smart Car for Arduino</small></h3>
+                <a href="#smart-car-details">Project Details</a>
+                <br>
+                <a href="https://github.com/stormBandit/juice-shop">See Project on Github</a>
+                <img class="img-responsive portfolio-item" src="images/sunfounder.jpeg" alt="">
+            </div>
+
+        </div>
+
+    </div>
 
 <?php include("includes/footer.html"); ?>
-
--- acknowledgements.php
+++ acknowledgements.php
@@ -1,6 +1,7 @@
 <?php include("includes/header.html"); ?>
 
     <h1>image sources:</h1>
-    <h1>https://www.linkedin.com/company/tulip-retail</h1>
+    <h1>Tulip Retail -> https://www.linkedin.com/company/tulip-retail</h1>
+    <h1>Juice Shop Project -> https://www.owasp.org/index.php/</h1>
 
 <?php include("includes/footer.html"); ?>
