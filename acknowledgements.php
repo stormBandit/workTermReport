0e1c82bbc72c96506b772e7fe599b41a975c7cac
dalton <dalton.polhill@tulip.io>
2017-08-10 11:34:16 -0400

    referenced sunfounder project

--- acknowledgements.php
+++ acknowledgements.php
@@ -3,5 +3,6 @@
     <h1>image sources:</h1>
     <h1>Tulip Retail -> https://www.linkedin.com/company/tulip-retail</h1>
     <h1>Juice Shop Project -> https://www.owasp.org/index.php/</h1>
+    <h1>SunFounder Project -> https://github.com/sunfounder</h1>
 
 <?php include("includes/footer.html"); ?>
