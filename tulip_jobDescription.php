707369ee4e563d9ccb876e9900c1baa6ed2683c6
dalton <dalton.polhill@tulip.io>
2017-08-21 15:49:34 -0400

    added header bar to tulip goals and tulip jobdescription pages

--- tulip_jobDescription.php
+++ tulip_jobDescription.php
@@ -1,5 +1,30 @@
 <?php include("includes/header.html"); ?>
 
+<div class="container">
+    <img class="page-header" src="images/">
+
+    <nav class="navbar" style="background-color: #F25E78; font: white" role="navigation">
+        <div class="container" >
+            <div class="row">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav" style="font: white">
+                        <li>
+                            <a style="color: white" href="tulip_about.php">About Tulip Retail</a>
+                        </li>
+                        <li>
+                            <a style="color: white;" href="tulip_jobDescription.php">Job Description</a>
+                        </li>
+                        <li>
+                            <a style="color: white" href="tulip_goals.php">Work Term Goals</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+
+</div>
 
 
 <?php include("includes/footer.html"); ?>
