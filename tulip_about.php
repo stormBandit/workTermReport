c300778889a0dfa484820795801040d3c23fca19
dalton <dalton.polhill@tulip.io>
2017-08-23 17:36:46 -0400

    started the tulip about description

--- tulip_about.php
+++ tulip_about.php
@@ -2,6 +2,10 @@
 <?php include("includes/tulip_header.php"); ?>
 
 <div class="container">
+    <p>Tulip Retail has a innovative product that is  a powerful cloud-based, mobile platform
+        built to empower store associates.Tulip brings the best of online retail to brick-and-mortar
+        stores by delivering a powerful application that enhances the in-store experience. git
+    </p>
 
 </div>
 
