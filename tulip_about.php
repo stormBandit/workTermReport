c47910e548903d4a255dc4ebb0db9c7ef93f6803
dalton <dalton.polhill@tulip.io>
2017-08-23 08:20:42 -0400

    added the header to tulip header and removed from files, to isolate the header

--- tulip_about.php
+++ tulip_about.php
@@ -1,31 +1,7 @@
 <?php include("includes/header.html"); ?>
+<?php include("includes/tulip_header.php"); ?>
 
 <div class="container">
-    <img class="page-header" src="images/">
-
-    <nav class="navbar" style="background-color: #F25E78; font: white" role="navigation">
-        <div class="container" >
-            <div class="row">
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav" style="font: white">
-                        <li>
-                            <a style="color: white" href="tulip_about.php">About Tulip Retail</a>
-                        </li>
-                        <li>
-                            <a style="color: white;" href="tulip_jobDescription.php">Job Description</a>
-                        </li>
-                        <li>
-                            <a style="color: white" href="tulip_goals.php">Work Term Goals</a>
-                        </li>
-                        <li>
-                            <a style="color: white" href="tulip_conclusion.php">Conclusion</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
-
 
 </div>
 
