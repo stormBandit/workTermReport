be615e8f2988daf85273ec409fdfb7540151a335
dalton <dalton.polhill@tulip.io>
2017-08-24 14:17:45 -0400

    added the font size medium to be a css config

--- tulip_about.php
+++ tulip_about.php
@@ -3,13 +3,13 @@
 
 <div class="container">
     <div class="col-md-12">
-        <p style="font-size: medium">
+        <p>
             Tulip Retail has an innovative product, a powerful cloud-based, mobile platform
             built to empower store associates. Tulip brings the best of online retail to brick-and-mortar
             stores by delivering a powerful application that enhances the in-store experience.
         </p>
         <img src="images/tulip_tweet.png" class="pull-left" style="width: 250px; height: auto; padding-right: 10px">
-        <p style="font-size: medium">
+        <p>
             The iOS app used by store associates is an extremely powerful tool to connect associates with
             customers in a brand new way. The idea of introducing omnichannel avenues into a brick-and-mortar
             store has been revolutionary thus far and continues to pick up momentum. Tulip has become a leader in
@@ -17,7 +17,7 @@
             industry leader in the market space, with nobody else doing what Tulip is doing.
         </p>
         <img src="images/ali_asaria.jpg" class="pull-right" style="width: 300px; height: auto; padding-left: 10px">
-        <p style="font-size: medium">
+        <p>
             Ali Asaria, CEO of Tulip Retail, first founded the company <a href="https://well.ca">well.ca</a> before
             moving on to found Tulip. Well.ca is an online health, beauty and pharmaceutical retailer based entirely
             in Canada. Ali was also the man behind the BrickBreaker game found on BlackBerry devices from his days
