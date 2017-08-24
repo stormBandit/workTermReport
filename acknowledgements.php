91f5e63c1d41dd076ad6f236b957182625020d2c
dalton <dalton.polhill@tulip.io>
2017-08-24 11:25:02 -0400

    worked on tulip about page

--- acknowledgements.php
+++ acknowledgements.php
@@ -2,7 +2,10 @@
 
     <h1>image sources:</h1>
     <h1>Tulip Retail -> https://www.linkedin.com/company/tulip-retail</h1>
-    <h1>Juice Shop Project -> https://www.owasp.org/index.php/</h1>
+<h1>Tulip Twee -> https://twitter.com/tulipretail?lang=en </h1>
+<h1> Ali -> http://www.cbc.ca/news/canada/kitchener-waterloo/tulip-retail-ali-asaria-expanding-100-jobs-1.3685289</h1>
+
+        <h1>Juice Shop Project -> https://www.owasp.org/index.php/</h1>
     <h1>SunFounder Project -> https://github.com/sunfounder</h1>
 
 <?php include("includes/footer.html"); ?>
--- tulip_about.php
+++ tulip_about.php
@@ -2,10 +2,29 @@
 <?php include("includes/tulip_header.php"); ?>
 
 <div class="container">
-    <p>Tulip Retail has a innovative product that is  a powerful cloud-based, mobile platform
-        built to empower store associates.Tulip brings the best of online retail to brick-and-mortar
-        stores by delivering a powerful application that enhances the in-store experience. git
-    </p>
+    <div class="col-md-12">
+        <p style="font-size: medium">
+            Tulip Retail has an innovative product, a powerful cloud-based, mobile platform
+            built to empower store associates. Tulip brings the best of online retail to brick-and-mortar
+            stores by delivering a powerful application that enhances the in-store experience.
+        </p>
+        <img src="images/tulip_tweet.png" class="pull-left" style="width: 250px; height: auto; padding-right: 10px">
+        <p style="font-size: medium">
+            The iOS app used by store associates is an extremely powerful tool to connect associates with
+            customers in a brand new way. The idea of introducing omnichannel avenues into a brick-and-mortar
+            store has been revolutionary thus far and continues to pick up momentum. Tulip has become a leader in
+            arming retailers with important data and tools for success. Through this process Tulip has become an
+            industry leader in the market space, with nobody else doing what Tulip is doing.
+        </p>
+        <img src="images/ali_asaria.jpg" class="pull-right" style="width: 300px; height: auto; padding-left: 10px">
+        <p style="font-size: medium">
+            Ali Asaria, CEO of Tulip Retail, first founded the company <a href="https://well.ca">well.ca</a> before
+            moving on to found Tulip. Well.ca is an online health, beauty and pharmaceutical retailer based entirely
+            in Canada. Ali was also the man behind the BrickBreaker game found on BlackBerry devices from his days
+            working at Research in Motion. Ali started Tulip Retail in Waterloo, ON in 2013 and has quickly grown in
+            size to almost 200 employees across Canada, the U.S. and Europe.
+        </p>
+    </div>
 
 </div>
 
