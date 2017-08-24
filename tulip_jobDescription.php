3ab8e952710e0ac4cbcf8b0a5a9c7b80a62c2c34
dalton <dalton.polhill@tulip.io>
2017-08-24 11:40:58 -0400

    worked on tulip job desc

--- tulip_jobDescription.php
+++ tulip_jobDescription.php
@@ -2,7 +2,34 @@
 <?php include("includes/tulip_header.php"); ?>
 
 <div class="container">
-
+    <p>
+        Official Title: Full-Stack Developer
+        <br><br>
+        Description: <br>
+        We are looking for enthusiastic software developers who aren't afraid to take on big
+        and exciting challenges. We are a small, growing team of engineers that believe in deploying
+        often and fast, and pushing each other to become better at our craft. As a software developer,
+        you will design and build a complete solution, including the latest in web and mobile technology.
+        You will be part of a mid-sized startup that is poised to break in to a massive untapped space.
+        <br><br>
+        My role:<br>
+        * Work with an agile and highly skilled development team<br>
+        * Build and design RESTful APIs<br>
+        * Create elegant and functional mobile and web applications<br>
+        * Integrate the Tulip platform with the retail systems of world class brands
+        <br><br>
+        Expectations:<br>
+        * Reading and interpreting big data<br>
+        * Experience with common web technologies, such as: PHP, mySQL, nginx, redis, Python<br>
+        * Thoughtful problem solvers who can simplify complex processes<br>
+        * Experience building and shipping iOS applications is a plus<br>
+        * No jerks
+        <br><br>
+        My Time at Tulip:<br>
+        During my 4 month co-op at Tulip I worked on a team which serviced the client <a href="https://well.ca">well.ca</a>
+        the team consisted of myself, another co-op and our manager. The small team dynamic meant that we all had to
+        be able to deal with all components of the very large and intricate website.
+    </p>
 </div>
 
 
