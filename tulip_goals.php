2dcd2a7c1c0d8c201d99a238fdcdae0a0942429b
dalton <dalton.polhill@tulip.io>
2017-08-24 15:10:23 -0400

    created the panel groups for the goals, built the first one

--- tulip_goals.php
+++ tulip_goals.php
@@ -3,6 +3,75 @@
 
     <div class="container">
 
+        <!-- Goal #1 -->
+        <div class="panel-group" style="width: 750px;">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" href="#collapse1">Goal #1 - Develop Knowledge of how websites work</a>
+                    </h4>
+                </div>
+                <div id="collapse1" class="panel-collapse collapse">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#goal1-description">Goal</a>
+                                </h4>
+                            </div>
+                            <div id="goal1-description" class="panel-body collapse">
+                                <p>
+                                    I would like to gain a much more indepth understanding of how web sites work with a
+                                    server and a database in order to create an efficient and effective environment. I want
+                                    to expand my knowledge of the different languages used in web development such as Php,
+                                    javascript, css, jquery and I would like a deeper understand of mySql.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#goal1-action-plan">Action Plan</a>
+                                </h4>
+                            </div>
+                            <div id="goal1-action-plan" class="panel-body collapse">
+                                <p>
+                                    I will accomplish this goal by asking many questions to my manager and team, when I am
+                                    unsure of some and for clarification of processes. I would also like to check in with my manager when I
+                                    accomplish tasks to ensure that I am doing them in the most effective/efficient way possible.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#goal1-measure-success">Measure of Success</a>
+                                </h4>
+                            </div>
+                            <div id="goal1-measure-success" class="panel-body collapse">
+                                <p>
+                                    I will measure this goal by building a simplistic website of my own and seeing how easily I
+                                    am able to do so, aquiring more knowledge and practice wiht the parts I struggle with.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#goal1-reflection">Reflection</a>
+                                </h4>
+                            </div>
+                            <div id="goal1-reflection" class="panel-body collapse">
+                                <p>
+                                    Nothing.
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
     </div>
 
 <?php include("includes/footer.html"); ?>
