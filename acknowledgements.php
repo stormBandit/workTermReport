44429cc4809d4c78c9ffdde20bc1d1e5656d6d50
dalton <dalton.polhill@tulip.io>
2017-08-31 07:52:05 -0400

    built the acknowledgments page

--- acknowledgements.php
+++ acknowledgements.php
@@ -1,10 +1,36 @@
 <?php include("includes/header.html"); ?>
 
-    <h1>image sources:</h1>
-    <h1>Tulip Retail -> https://www.linkedin.com/company/tulip-retail</h1>
-    <h1>Tulip Twee -> https://twitter.com/tulipretail?lang=en </h1>
-    <h1> Ali -> http://www.cbc.ca/news/canada/kitchener-waterloo/tulip-retail-ali-asaria-expanding-100-jobs-1.3685289</h1>
-    <h1>Juice Shop Project -> https://www.owasp.org/index.php/</h1>
-    <h1>SunFounder Project -> https://github.com/sunfounder</h1>
+    <div class="container">
+        <div class="page-header">
+            <h1>Thank You!</h1>
+        </div>
+        <div class="pager" style="text-align: left;">
+            <p>There are many people I would like to thank for the support and advice I have been given
+                over the course of my post-secondary career. First and foremost a big thanks to
+                Laura Gatto, my co-op coordinator who has helped me throughout my career as a co-op
+                student. Laura helped me enter into the co-op program, build a comprehensive resume
+                which helped me land a job as an undergraduate research assistant even before my first
+                co-op work term. Laura has been there to support me during my co-op terms and is always
+                happy to help with any questions or concerns I may have. She is a great mentor and friend!
+                <br><br>
+                I would also like to thank Greg Klotz, the coordinator for the school of computer science.
+                Greg has been a tremendous help when it came to choosing courses and deciding my career path,
+                Greg was able to guide me into the proper courses that would benefit me most in what I wished
+                to do after graduation.
+            </p>
+        </div>
+
+        <div class="page-header">
+            <h1>image sources:</h1>
+        </div>
+        <div class="pager" style="text-align: left">
+            <p>Tulip Retail -> <a href="https://www.linkedin.com/company/tulip-retail">https://www.linkedin.com/company/tulip-retail</a></p>
+            <p>Tulip Tweet -> <a href="https://twitter.com/tulipretail?lang=en">https://twitter.com/tulipretail?lang=en</a></p>
+            <p> Ali -> <a href="http://www.cbc.ca/news/canada/kitchener-waterloo/tulip-retail-ali-asaria-expanding-100-jobs-1.3685289">http://www.cbc.ca/news/canada/tulip-retail-ali-asaria</a></p>
+            <p>Juice Shop Project -> <a href="https://www.owasp.org/index.php/">https://www.owasp.org/index.php/</a></p>
+            <p>SunFounder Project -> <a href="https://github.com/sunfounder">https://github.com/sunfounder</a></p>
+        </div>
+
+    </div>
 
 <?php include("includes/footer.html"); ?>
